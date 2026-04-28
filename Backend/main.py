ecc3517e255996af05db55eb30b717be32671be0
Muhammad Sameer <samadarkhan196@gmail.com>
2026-04-28 11:06:07 +0500

    Initialize FastAPI app with CORS and DB setup
    Set up FastAPI application with CORS middleware and database table creation.

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from Backend.db import Base, engine
+from Backend.routes.auth import router
 
+app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For development only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# create tables
+Base.metadata.create_all(bind=engine)
+
+
+app.include_router(router)
